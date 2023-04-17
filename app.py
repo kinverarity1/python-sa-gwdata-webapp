5a3cc4a0d4f9e1f72ba06fbd58f42e3613132dc0
Kent Inverarity <kent@nowhere>
2023-04-17 15:32:41 +0930

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+
+from fastapi import Depends, Request, FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+
+import pandas as pd
+import numpy as np
+
+import sa_gwdata as gd
+
+
+app = FastAPI(debug=True)
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/app/well/{dh_no}")
+def well(request: Request, dh_no: int):
+    summ = gd.wells_summary([dh_no])
+    return templates.TemplateResponse(
+        "well.html",
+        context={
+            "request": request,
+            "summ": summ,
+        },
+    )
+
+
+@app.get("/app/identify/{input}")
+def identify(request: Request, input: str):
+    wells = gd.find_wells(input)
+    return RedirectResponse(f"/app/well/{wells[0].dh_no}")

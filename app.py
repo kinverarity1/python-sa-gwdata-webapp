29ca4001d583f89a743f3271c0bb2a1fb9674139
Kent Inverarity <kinverarity1@users.noreply.github.com>
2023-04-19 11:19:47 +0930

    Add application logger

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ import numpy as np
 
 import sa_gwdata as gd
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 app = FastAPI(debug=True)
 templates = Jinja2Templates(directory="templates")
@@ -20,9 +24,14 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 def well(request: Request, dh_no: int):
     summ = gd.wells_summary([dh_no])
     well_summary = summ.iloc[0].to_dict()
+    well = gd.Well(
+        dh_no=well_summary["dh_no"],
+        obs_no=well_summary["obs_no"],
+        unit_no=well_summary["unit_long"],
+    )
     context = {
         "request": request,
-        "title": gd.make_well_title(**well_summary),
+        "title": well.title,
         "summary_table": summ.T.to_html(),
     }
     context.update(well_summary)
